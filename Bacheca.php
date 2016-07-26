29394bfb5b8f09ddd4c65617e73d2785c1cb8865
federico <federico@Fede-PC.lan>
2016-07-26 22:54:05 +0200

    Riordinata la pagina, cancellate le vecchie query e copiate su un file. Prova del sito con la nuova query sql e la stampa. Probabilmente senza commit e push upstream non si riesce a provare il sito modificato

--- Bacheca.php
+++ Bacheca.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
-
 require_once("database.php");
-session_set_cookie_params(0, "/", ".cinguettio.it");
 if (!isset($_SESSION['email']) || !isset($_SESSION['pwd'])) {
     echo "Non sei connesso!";
     die;
-} else {
-    echo "Sei connesso!";
+} 
+?>
     // TODO
     // STEP 1
     // recuperare l'utente loggato da $_SESSION['email']
@@ -17,67 +15,41 @@ if (!isset($_SESSION['email']) || !isset($_SESSION['pwd'])) {
     // fare un loop per stampare l'id e il tipo di cinguettio
     // STEP 2
     // per ogni id di cinguettio e tipo preleviamo le info dalla tabella relativa
-    // e stampiamo il contenuto.
-}
-?>
+    // e stampiamo il contenuto
 
-<html>
-    <title>Bacheca</title>
-    <head>
-        <link rel="stylesheet" type="text/css" href="css.css">
-    </head>
-    <body>
-        <ul>
-            <li><a class="active" href="Bacheca.php" class='active'>Bacheca</a></li>
-            <li><a href="Datiutente.php">Dati Utente</a></li>
-            <li><a href="Chiseguo.php">Chi Seguo</a></li>
-            <li><a href="Chimisegue.html">Chi Mi Segue</a></li>
-            <li><a href="Logout.php">Logout</a></li>
-        </ul>
-        <br>
-        <br>
-        <form method="post" action="Bacheca.php">
-            <textarea name="cintesto" rows="2" cols="100" placeholder="Cinguettio" autofocus></textarea>
-            <input type="submit" value="Invia">
-        </form>
-
-<?php
-if (isset($_POST['Invia'])) {
-
-    $sql = "SELECT MAX(IdProg) FROM Cinguettio c,Utente u WHERE c.email=u.email";
-    $res = mysql_query($sql, $cid);
-    if (is_null($res) == TRUE)
-        $contaid = 0;
-    else
-        $contaid = $contaid + 1;
-
-    $testo = addslashes($_POST['cintesto']);
-    $id = addslashes($contaid);
-    $email = addslashes($_POST['mail']);
-    $data = addslashes(date("d/m/Y"));
-    $sql = "insert into cinguettio (IdProg, Email, DataCreazione) values ('$id', '$email', '$data');";
-    $res = mysql_query($sql, $cid) or die("Errore " . mysql_error());
-    $sql = "insert into testo (IdProg, Testo) values ('$id', '$testo');";
-    $res = mysql_query($sql, $cid) or die("Errore " . mysql_error());
-}
-?>
-
-        Lista Cinguettii pubblicati in ordine (in alto meno recente)
-
-<?php
-$sql = "SELECT * FROM Cinquettio NATURAL JOIN Testo";
-$res = mysql_query($sql, $cid);
-if ($res && mysql_num_rows($res) > 0) {
-    while ($row = mysql_fetch_assoc($res)) {
-        echo $row[‘ID’]." <br> ";
-					echo $row[‘Testo’]." <br> ";
-        echo $row[‘Data’]." <br><br><br>
+<title>Bacheca</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="Bacheca.php" class='active'>Bacheca</a></li>
+        <li><a href="Datiutente.php">Dati Utente</a></li>
+        <li><a href="Chiseguo.php">Chi Seguo</a></li>
+        <li><a href="Chimisegue.html">Chi Mi Segue</a></li>
+        <li><a href="Logout.php">Logout</a></li>
+    </ul>
+    <br>
+    <br>
+    <form method="post" action="Bacheca.php">
+        <textarea name="cintesto" rows="2" cols="100" placeholder="Cinguettio" autofocus></textarea>
+        <input type="submit" value="Invia">
+    </form>
         
-            ";
-				}
-			} else
-			echo "nessun risultato";
-		?>
-		
-	</body>
-</html>
+    <?php    $email=$_SESSION[email];
+    
+    $sql= "SELECT DISTINCT u.email, c.id, l.nomeL
+            FROM segue s, utente u, luogo l, cinguettio c
+            WHERE (s.utenteCheSegue=$email AND s.utenteSeguito=u.email 
+                AND c.email=u.email AND c.id=l.id)
+            OR (s.utenteCheSegue=$email AND s.utenteCheSegue=u.email
+                AND c.email=u.email AND c.id=l.id)	
+            ORDER BY c.dataOraCreazione DESC";
+    
+        if ($result = mysqli_query($mysqli, $sql)) {
+            printf("Select returned %d rows.\n", mysqli_num_rows($result));
+            mysqli_free_result($result);
+        }
+    ?>
+</body>
+
