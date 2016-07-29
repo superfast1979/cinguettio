9680963e31edf639aaf9d9ecb4b181da0d62709b
superfast1979 <marco.augello@gmail.com>
2016-07-29 23:12:48 +0200

    Ricerca utenti Rimozione utente Aggiunta utenti Pagina Chiseguo

--- Chiseguo.php
+++ Chiseguo.php
@@ -12,6 +12,33 @@ $panel = "panel panel-foto";
 
 $_SESSION['title'] = "Chi Seguo";
 include("head.php");
+
+if (isset($_GET['elimina'])) {
+    // creare una select degli utenti con le opzioni di ricerca, se presenti.
+    // inizializzare un array associativo per valorizzare la lista di utenti
+    // nel panel della ricerca con icona aggiuni
+}
+
+if (isset($_GET['aggiungi'])) {
+    // fare update DB per aggiunta associazione $_GET['utente'] negli utenti seguiti da $email
+}
+
+if (isset($_GET['elimina'])) {
+    // fare update DB per rimozione $_GET['utente'] dagli utenti seguiti da $email
+}
+
+$sql = "SELECT utenteSeguito FROM segue WHERE utenteCheSegue='$email'";
+$seguiti = array();
+if ($result = db_query($sql)) {
+//    printf("$br Select returned %d rows.\n", mysqli_num_rows($result));
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($seguiti, $row);
+//        printf("$br %s %s %s", $row['email'], $row['dataOraCreazione'], $row['stringaDaStampare']);
+    }
+    mysqli_free_result($result);
+} else {
+    printf(db_error());
+}
 ?>
 
 <body>
@@ -30,44 +57,44 @@ include("head.php");
         </div>
     </nav>
 
-    <?php
-    $sql = "SELECT utenteSeguito FROM segue WHERE utenteCheSegue='$email'";
-    $seguiti = array();
-    if ($result = db_query($sql)) {
-//    printf("$br Select returned %d rows.\n", mysqli_num_rows($result));
-        while ($row = mysqli_fetch_assoc($result)) {
-            array_push($seguiti, $row);
-//        printf("$br %s %s %s", $row['email'], $row['dataOraCreazione'], $row['stringaDaStampare']);
-        }
-        mysqli_free_result($result);
-    } else {
-        printf(db_error());
-    }
-    
-    
-    ?>
     <div class = "row">
-        <?php
-    for ($i = 0; $i < count($seguiti); $i++) {
-        ?>
-
-            <div class="col-md-4">
+        <div class="col-md-4">
+            <?php
+            for ($i = 0; $i < count($seguiti); $i++) {
+                ?>
                 <div class="<?php echo $panel; ?>">
                     <div class="panel-body">
-    <?php echo $seguiti[$i]['utenteSeguito'] ?>
+                        <?php echo $seguiti[$i]['utenteSeguito'] ?>
+                        <a href="Chiseguo.php?elimina=true&utente=<?php echo $seguiti[$i]['utenteSeguito']; ?>">
+                            <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                        </a>
                     </div>
                 </div>
-            </div>
-        </div> <?php
-}
-?>
-    <div class="col-md-8">
-        <div class="<?php echo $panel; ?>">
-            <div class="panel-body">
-<?php echo $seguiti[$i]['utenteSeguito'] ?>
+                <?php
+            }
+            ?>
+        </div>
+        <div class = "col-md-8">
+            <div class="<?php echo $panel; ?>">
+                <div class="panel-body">
+                    Search
+                </div>
+                <?php
+                for ($i = 0; $i < count($seguiti); $i++) {
+                    ?>
+                    <div class="<?php echo $panel; ?>">
+                        <div class="panel-body">
+                            <?php echo $seguiti[$i]['utenteSeguito'] ?>
+                            <a href="Chiseguo.php?aggiungi=true&utente=<?php echo $seguiti[$i]['utenteSeguito']; ?>">
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                            </a>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>                
             </div>
         </div>
     </div>
-
 
 </body>
