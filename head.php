ccfdbe9ab117720cb24b69ad446d131659465f56
augello <augello@itmi00dw1427.corp.dom>
2016-08-29 23:23:05 +0200

    reso locale il file jquery

--- head.php
+++ head.php
@@ -1,10 +1,7 @@
 <head>
     <title><?php echo $_SESSION['title']; ?></title>
-    <!--    <link rel="stylesheet" type="text/css" href="css.css">-->
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/stile.css">
-    <script src=https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js></script>
+    <script src=js/jquery.min.js></script>
     <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-     
 </head>
